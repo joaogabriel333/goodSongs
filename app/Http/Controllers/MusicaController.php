ce12e671b2c0be2c0ef7f9a66593a6f85177f18d
joaogabriel333 <you@example.com>
2024-05-07 15:11:43 -0300

    atualizando

--- app/Http/Controllers/MusicaController.php
+++ app/Http/Controllers/MusicaController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class MusicaController extends Controller
 {
-    //
+    public function cadastro{MusicaRequest $request}{
+        $musica = musica::create([
+            'nome'=>$request->nome,
+            'duracao'=>$request->duracao,
+            'artista'=>$request->artista,
+            'genero'=>$request->genero,
+            'nacionalidade'=>$request->nacionalidade,
+            'ano_lançamento'=>$request->ano_lançamento,
+            'album'=>$request->album,
+        ]);
+        return response()-> json 
+    }
 }
